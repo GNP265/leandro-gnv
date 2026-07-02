c0bbfc554778ec427e689dca4e79a42965d4494e
GNP265 <103005677+GNP265@users.noreply.github.com>
2026-07-01 22:10:01 -0300

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -2,10 +2,15 @@ import os
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
+
 SECRET_KEY = os.environ.get('SECRET_KEY', 'dev-insecure-change-me-in-production')
 DEBUG = os.environ.get('DEBUG', 'False').lower() in ('true', '1')
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
-CSRF_TRUSTED_ORIGINS = [o for o in os.environ.get('CSRF_TRUSTED_ORIGINS', '').split(',') if o]
+ALLOWED_HOSTS = ['*']
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://*.vercel.app',
+    'https://leandro-gnv.vercel.app',
+]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -43,16 +48,17 @@ TEMPLATES = [{
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
-# Database
 db_url = os.environ.get('DATABASE_URL')
 if db_url:
     import dj_database_url
-    DATABASES = {'default': dj_database_url.parse(db_url)}
+    DATABASES = {'default': dj_database_url.parse(db_url, conn_max_age=600)}
 else:
     DATABASES = {'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': '/tmp/db.sqlite3',
     }}
+
+SESSION_ENGINE = 'django.contrib.sessions.backends.db'
 
 AUTH_PASSWORD_VALIDATORS = []
 LANGUAGE_CODE = 'pt-br'
@@ -62,6 +68,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [BASE_DIR / 'gestao' / 'static']
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
